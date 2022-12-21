a52490edd1b9959d0532997ab25668ef1ac8f3ae
Caffineandme <matthuffman26@gmail.com>
2022-12-20 20:28:06 -0600

    update site

---
+++ src/pages/Request.js
@@ -0,0 +1,16 @@
+export default function Request() {
+	return <h1>request go here</h1>;
+}
+
+/*import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>This page for maintenance request</h1>
+		</div>
+	);
+};
+
+export default Request;
+*/
